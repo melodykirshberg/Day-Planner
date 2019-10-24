e34407832cbe28474976f31f1e20b51271635cd3
verlitas <55808608+verlitas@users.noreply.github.com>
2019-10-24 14:10:13 -0700

    fixed styling

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var today = $("#current");
 
 setInterval(function () {
-    today.text(moment().format("MMM Do YYYY - hh:mm:ss "));
+    today.text(moment().format("MMM Do YYYY - hh:mm:ss"));
 }, 1000);
 
 
@@ -16,31 +16,31 @@ function getit() {
         todos = {};
         tstorage("localStoragetodos", todos);
     }
-    if (todos.hasOwnProperty("text9")) {
+    else if (todos.hasOwnProperty("text9")) {
         document.getElementById("text9").value = todos.t9;
     }
-    if (todos.hasOwnProperty("text10")) {
+    else if (todos.hasOwnProperty("text10")) {
         document.getElementById("text10").value = todos.t10;
     }
-    if (todos.hasOwnProperty("text11")) {
+    else if (todos.hasOwnProperty("text11")) {
         document.getElementById("text11").value = todos.t11;
     }
-    if (todos.hasOwnProperty("text12")) {
+    else if (todos.hasOwnProperty("text12")) {
         document.getElementById("text12").value = todos.test12;
     }
-    if (todos.hasOwnProperty("text13")) {
+    else if (todos.hasOwnProperty("text13")) {
         document.getElementById("text13").value = todos.test13;
     }
-    if (todos.hasOwnProperty("text14")) {
+    else if (todos.hasOwnProperty("text14")) {
         document.getElementById("text14").value = todos.test14;
     }
-    if (todos.hasOwnProperty("text15")) {
+    else if (todos.hasOwnProperty("text15")) {
         document.getElementById("text15").value = todos.test15;
     }
-    if (todos.hasOwnProperty("text16")) {
+    else if (todos.hasOwnProperty("text16")) {
         document.getElementById("text16").value = todos.test16;
     }
-    if (todos.hasOwnProperty("text17")) {
+    else if (todos.hasOwnProperty("text17")) {
         document.getElementById("text17").value = todos.test17;
     }
 }
