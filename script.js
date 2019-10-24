347a5c413d39f2995bea03b04405149e28f4a43f
verlitas <55808608+verlitas@users.noreply.github.com>
2019-10-24 15:15:59 -0700

    fixed script and got local storage to work

--- script.js
+++ script.js
@@ -7,65 +7,25 @@ setInterval(function () {
 
 
 
-var todos = [];
-
 function getit() {
 
-    todos = fstorage("localStoragetodos");
-    if (todos === null) {
-        todos = {};
-        tstorage("localStoragetodos", todos);
+    for(var i = 9; i < 18; i++){
+        if(localStorage.getItem("text" + i)){
+            $("#text" + i).val(localStorage.getItem("text" + i));
+        }
     }
-    else if (todos.hasOwnProperty("text9")) {
-        document.getElementById("text9").value = todos.t9;
-    }
-    else if (todos.hasOwnProperty("text10")) {
-        document.getElementById("text10").value = todos.t10;
-    }
-    else if (todos.hasOwnProperty("text11")) {
-        document.getElementById("text11").value = todos.t11;
-    }
-    else if (todos.hasOwnProperty("text12")) {
-        document.getElementById("text12").value = todos.test12;
-    }
-    else if (todos.hasOwnProperty("text13")) {
-        document.getElementById("text13").value = todos.test13;
-    }
-    else if (todos.hasOwnProperty("text14")) {
-        document.getElementById("text14").value = todos.test14;
-    }
-    else if (todos.hasOwnProperty("text15")) {
-        document.getElementById("text15").value = todos.test15;
-    }
-    else if (todos.hasOwnProperty("text16")) {
-        document.getElementById("text16").value = todos.test16;
-    }
-    else if (todos.hasOwnProperty("text17")) {
-        document.getElementById("text17").value = todos.test17;
-    }
+    
 }
 
-
-//---------------------------------creates value when clicking save
-function save(key, value) {
-    todos[key] = value;
-    tstorage("localStoragetodos", todos);
-
-}
-
-//--------------------------------sends new todos to local storage
-function tstorage(key, value) {
-    localStorage.setItem(key, JSON.stringify(value));
-}
-
-
-//--------------------------------retrieves todos from local storage
-function fstorage(key) {
-    return JSON.parse(localStorage.getItem(key));
-}
 
 $("button").on("click", function(){
-    save("text9", document.getElementById("text9").value);
-    getit();
+    var textarea = $(this).siblings("textarea");
+    var id = textarea.attr("id");
+    var value = textarea.val();
+
+    localStorage.setItem(id, value);
+
 
 })
+
+getit();
